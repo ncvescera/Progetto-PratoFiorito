76651a1b863dfc583f555d1e0001691a40f9f57d
Nicolò Vescera <slayercandy@icloud.com>
2016-05-18 10:43:24 +0200

    Aggiunta classe FIeld. Creato metodo per spawnare bombe a caso. Da controllare lo spawn

--- src/campo/minato/CampoMinato.java
+++ src/campo/minato/CampoMinato.java
@@ -4,7 +4,10 @@ package campo.minato;
 public class CampoMinato {
 
     public static void main(String[] args) {
-        new MineField();
+        //new MineField();
+        FIeld a = new FIeld(10,10);
+        a.init(10);
+        a.print();
     }
     
 }
---
+++ src/campo/minato/FIeld.java
@@ -0,0 +1,56 @@
+package campo.minato;
+
+import java.util.Random;
+
+public class FIeld {
+    private int matrice[][];
+    private final int  Y;
+    private final int X;
+    private final int BOMB = -7;
+    
+    public FIeld(int x, int y){
+        this.X = x;
+        this.Y = y;
+        
+        this.matrice = new int[Y][X];
+    }
+    
+    public void init(int iter){
+        int i,j;
+   
+        for(i=0;i<iter;i++){
+            int[] YX = this.spawnCell();
+            
+            this.matrice[YX[0]][YX[1]] = BOMB;
+        }
+        
+        for(i=0;i<Y;i++){
+            for(j=0;j<X;j++){
+                if(matrice[i][j]!= BOMB)
+                    matrice[i][j] = 0;
+            }
+        }
+        
+    }
+    
+    private int[] spawnCell(){
+        Random rand = new Random();
+        int[] array = new int[2];
+        
+        array[0] = rand.nextInt(this.Y);
+        array[1] = rand.nextInt(this.X);
+        
+        return array;
+    }
+    
+    public void print(){
+        int i,j;
+        
+        for(i=0;i<Y;i++){
+            for(j=0;j<X;j++){
+                System.out.print(matrice[i][j]+"\t");
+            }
+            System.out.print("\n");
+        }
+    }
+}
